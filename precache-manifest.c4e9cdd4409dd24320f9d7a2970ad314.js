5d6a9c8543f06ab171ef914bf128201a2f3253ab
Dillon Phillips <edmond.dillon.phillips@gmail.com>
2020-05-29 20:27:11 -0400

    Updates

---
+++ precache-manifest.c4e9cdd4409dd24320f9d7a2970ad314.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c0651dfe5ffc22999f0d55e2e82e2b9",
+    "url": "/GroceryStore_FrontEnd/index.html"
+  },
+  {
+    "revision": "67f5312d51f5c9874c3c",
+    "url": "/GroceryStore_FrontEnd/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44c55e6e65171b7d0dec",
+    "url": "/GroceryStore_FrontEnd/static/js/2.807a0ae0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GroceryStore_FrontEnd/static/js/2.807a0ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f5312d51f5c9874c3c",
+    "url": "/GroceryStore_FrontEnd/static/js/main.6c3429d2.chunk.js"
+  },
+  {
+    "revision": "b8908bc60182cf5ea0a9",
+    "url": "/GroceryStore_FrontEnd/static/js/runtime-main.95dfb630.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GroceryStore_FrontEnd/static/media/logo.5d5d9eef.svg"
+  }
+]);
